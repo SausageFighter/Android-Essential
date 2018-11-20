8ec759fbbc0d4e5be7229e9818cbc515e2934b9e
Mihai Olaru <mihaialexds@gmail.com>
2018-11-20 20:25:18 +0200

    first commit

---
+++ app/src/main/java/mihaiolaru/com/myfirstapplication/week3/LoginScreen.java
@@ -0,0 +1,15 @@
+package mihaiolaru.com.myfirstapplication.week3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mihaiolaru.com.myfirstapplication.R;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
