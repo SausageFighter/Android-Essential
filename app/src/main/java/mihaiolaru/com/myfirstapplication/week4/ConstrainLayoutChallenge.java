48e82470ad857406013ba183f738f9e842538f81
Mihai Olaru <mihaialexds@gmail.com>
2018-11-28 12:52:10 +0200

    Week4 constraint layout challenge

---
+++ app/src/main/java/mihaiolaru/com/myfirstapplication/week4/ConstrainLayoutChallenge.java
@@ -0,0 +1,15 @@
+package mihaiolaru.com.myfirstapplication.week4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mihaiolaru.com.myfirstapplication.R;
+
+public class ConstrainLayoutChallenge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constrain_layout_challenge);
+    }
+}
